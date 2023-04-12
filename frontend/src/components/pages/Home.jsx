992df1c551a86a5c9484e76bfc1f49777db8f3dd
MindofSkk <helpmeskk@gmail.com>
2023-04-12 22:15:15 +0530

    some changes

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -3,11 +3,12 @@ import React from "react";
 export const Home = () => {
   return (
     <div>
-      <p> library management system. </p>
+      {/* <p> library management system. </p> */}
       <img
         src="https://www.skoolbeep.com/blog/wp-content/uploads/2020/12/WHY-CONSIDER-SKOOLBEEP-THE-BEST-SCHOOL-LIBRARY-AUTOMATION-SOFTWARE-min.png" width={"100%"}
         alt=""
       />
+      
     </div>
   );
 };
